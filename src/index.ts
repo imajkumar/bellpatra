95d06c5d663e2c1b6abdd3a172d1e8411ec4e066
Ajay <ajayit2020@gmail.com>
2024-02-19 02:20:03 +0530

    Added00u

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = process.env.PORT ?? 3000;
 // Define Routes
 app.get("/", (req, res) =>
   res.json({
-    message: "Hello from Express Server imajkmar 👋🏻",
+    message: "Hello from Express Server 👋🏻",
   })
 );
 
