b625ed9b496f6b33e9bf4509eb35f001004cdf9d
Ajay <ajayit2020@gmail.com>
2024-02-24 20:35:32 +0530

    init

--- config/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    origin: 'http://localhost:8000',
-    accessTokenExpiresIn: 15,
-    refreshTokenExpiresIn: 60,
-    redisCacheExpiresIn: 60,
-    emailFrom: 'contact@codevoweb.com',
-  };
--- src/utils/allowOrigins.ts
+++
@@ -1,5 +0,0 @@
-export const allowedOriginsSystem = [
-    'https://www.yoursite.com',
-    'http://localhost:5500',
-    'http://localhost:3500'
-];
