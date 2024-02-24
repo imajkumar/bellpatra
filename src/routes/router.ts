4c959f004e18b9b9858ae6629d8a4e07931617e2
Ajay <ajayit2020@gmail.com>
2024-02-24 20:47:47 +0530

    init

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -3,7 +3,7 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) =>
-  res.json({ message: "Hello from Docker v39 🎉" }),
+  res.json({ message: "Hello from Docker v399 🎉" }),
 );
 
 router.get("/health", (req, res) => {
