8f7f9c4009d57a0f7153f0c6179b0b6911a33d2e
Ajay <ajayit2020@gmail.com>
2024-02-25 15:45:06 +0530

    added

--- src/database/config.ts
+++ src/database/config.ts
@@ -5,9 +5,9 @@ export const AppDataSource = new DataSource({
     type: "postgres",
     host: "localhost",
     port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'postgres',
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
     entities: [__dirname + "/../**/*.entity{.ts,.js}"],
     synchronize: true,
     logging: true 
