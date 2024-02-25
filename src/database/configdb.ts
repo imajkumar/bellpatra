dd7f42902c23630d48b386302bec1d2ab435831b
Ajay <ajayit2020@gmail.com>
2024-02-25 16:26:36 +0530

    added

--- src/database/configdb.ts
+++ src/database/configdb.ts
@@ -1,1 +1,12 @@
-console.log(999);
+import { DataSource } from "typeorm";
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "my_postgres",
+    port:5432,
+    username: "postgres",
+    password: "postgres",
+    database: "postgres",
+    entities: [__dirname + "/../**/*.entity{.ts,.js}"],
+    synchronize: false,
+    logging:false
+  });
