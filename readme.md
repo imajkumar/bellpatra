994b341bd8dc940766fbef8e0936115f971f62e5
Ajay <ajayit2020@gmail.com>
2024-02-25 12:30:17 +0530

    added

---
+++ readme.md
@@ -0,0 +1,2 @@
+npm install dotenv reflect-metadata typeorm config @types/node
+npm install config
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (req, res) =>
   res.json({
-    message: "Hello from Docker this is ayra  🎉",
+    message: "Hello from Docker this is ay9ra  🎉",
   }),
 );
 
