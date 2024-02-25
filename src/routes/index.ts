a3296abd86dc9e37659892cf90c628b7d70e0651
Ajay <ajayit2020@gmail.com>
2024-02-25 20:20:14 +0530

    added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,20 @@
 import express, { Request, Response } from "express";
+import redisClient from '../utils/connectRedis'
 
 var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req: Request, res: Response) {
   res.send("API : HOME -99 ");
+});
+
+router.get('/checkme', async (req:Request,res:Response) => {
+  const message = await redisClient.get('try');
+
+  res.status(200).json({
+    status: 'success',
+    message,
+  });
 });
 
 export default router;
