17ded3c49856230dc909d98dfe945f182692c8ba
Ajay <ajayit2020@gmail.com>
2024-02-25 14:10:37 +0530

    added

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,7 +4,7 @@ const router = Router();
 
 router.get("/", (req, res) =>
   res.json({
-    message: "Hello from Docker this is ay96ra  🎉",
+    message: "Hello from Docker this is ok  🎉",
   }),
 );
 
