8c0725ed9ca7f5120ad71e08b61aefd6ac082c39
Ajay <ajayit2020@gmail.com>
2024-02-25 15:24:55 +0530

    added

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,1 +1,5 @@
-console.log(777)
+import * as dotenv from "dotenv";
+import { DataSource } from "typeorm";
+dotenv.config();
+
+const { POSTGRES_HOST, POSTGRES_PORT, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, NODE_ENV } = process.env;
